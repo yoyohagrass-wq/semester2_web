d2f32eb8bcbbafc8262a1cbb4b99ab1f35605208
midos1nsane <mohamed.elnazer1442007@gmail.com>
2026-03-16 22:19:26 +0200

    Redirect to index page upon successful login and add new user to the users list

--- Website/Website/login.php
+++ Website/Website/login.php
@@ -29,10 +29,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                             $_SESSION["userid"] = $userid;
                             $_SESSION["username"] = $username;
                             $_SESSION["useremail"] = $useremail;
-                            $message = "Login successful. Welcome back to Al Mesbah Al Modie Foundation.";
-                            $messageClass = "success";
-                            $found = true;
-                            break;
+                            header("Location: index.php");
+                            exit;
                         }
                     }
                 }
