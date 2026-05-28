d05a551987632da0a4f0836bdc863920c23b16ea
midos1nsane <mohamed.elnazer1442007@gmail.com>
2026-05-28 22:38:15 +0300

    big black teez

--- Website/functions.php
+++ Website/functions.php
@@ -13,10 +13,11 @@ function pageHead($title, $description, $keywords) {
     <meta name="robots" content="index, follow">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
+    <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="home.css">
     <script src="script.js" defer></script>
 </head>
-<body class="site">
+<body>
 <?php
 }
 
