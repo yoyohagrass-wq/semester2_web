a065f29957eac25e5d715ce8747016814b958ad4
yoyohagrass-wq <yoyo.hagrass@gmail.com>
2026-06-01 01:35:14 +0300

    .

--- Admin/search_db.php
+++ Admin/search_db.php
@@ -4,14 +4,14 @@ $Conn = new mysqli("localhost", "root", "", "volunteer_db", 3307);
 $UserToSearch = $_REQUEST["name"];
 
 $SQL = "SELECT * FROM volunteers 
-        WHERE name LIKE '%$UserToSearch%'";
+        WHERE FullName LIKE '%$UserToSearch%'";
 
 $Result = $Conn->query($SQL);
 
 while ($Row = $Result->fetch_assoc()) {
-    echo "Name: " . $Row["name"] . "<br>";
-    echo "Email: " . $Row["email"] . "<br>";
-    echo "Phone: " . $Row["phone"] . "<br>";
-    echo "Message: " . $Row["message"] . "<br><hr>";
+    echo "Name: " . $Row["FullName"] . "<br>";
+    echo "Email: " . $Row["Email"] . "<br>";
+    echo "Phone: " . $Row["PhoneNumber"] . "<br>";
+    echo "Message: " . $Row["Message"] . "<br><hr>";
 }
 ?>
